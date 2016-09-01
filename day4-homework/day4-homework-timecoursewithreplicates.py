1865c85bff71319f8d1d458a43295447ce1359c8
Elizabeth Urban <lurban5@hotmail.com>
2016-09-01 18:06:53 -0400

    day4 homework correct version part 1

--- day4-homework/day4-homework-timecoursewithreplicates.py
+++ day4-homework/day4-homework-timecoursewithreplicates.py
@@ -12,12 +12,14 @@ ctab_dir = sys.argv[3] #/users...results/stringtie/
 #make female plot from paper
 #[1, 0, 2, 10, 50, 100]
 
-
+female_Sxl = []
 male_Sxl = []
 male_Sxl_replicates = []
 male_Sxl_replicates_stages = [4, 5, 6, 7]
 df_roi = df_meta["sex"] == "male"
 df2_roi = df_replicates["sex"] == "male"
+df3_roi = df_meta["sex"] == "female"
+
 
 for sample in df_meta[df_roi]["sample"]:
     filename = ctab_dir + "/" + sample + "/t_data.ctab"
@@ -25,6 +27,13 @@ for sample in df_meta[df_roi]["sample"]:
     #then appending the info from the new file to a list to sift through the fpkm values of just the females 
     df_roi2 = df["t_name"] == "FBtr0331261"
     male_Sxl.append(df[df_roi2]["FPKM"].values) #.values comes from np through pandas-it is taking a pandas series structure and converting it to a np structure so matpotlib can understand it-returns just the value without any of the extra names with it. 
+for sample in df_meta[df3_roi]["sample"]:
+    filename = ctab_dir + "/" + sample + "/t_data.ctab"
+    df3 = pd.read_table( filename ) 
+    
+    df_roi2 = df["t_name"] == "FBtr0331261"
+    female_Sxl.append(df3[df_roi2]["FPKM"].values)     
+
 for sample in df_replicates[df2_roi]["sample"]:
     filename2 = ctab_dir + "/" + sample + "/t_data.ctab"
     df2 = pd.read_table(filename2)
@@ -41,9 +50,11 @@ plt.figure()
 plt.title("Sxl male")
 #plt.xscale(10,2)
 plt.plot(male_Sxl)
+plt.plot(female_Sxl)
 plt.xticks(range(8), xticks, rotation="vertical")
+plt.ylim((0, 150))
 plt.scatter(male_Sxl_replicates_stages, male_Sxl_replicates)
 plt.xlabel("developmental stage")
 plt.ylabel("mRNA abundance (FPKM)")
-plt.savefig("timecourse_replicates.png")
+plt.savefig("timecourse_replicates2.png")
 plt.close()
