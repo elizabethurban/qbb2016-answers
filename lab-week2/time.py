c21b637ec73a8d58b909220b3e9d17b439d76904
Elizabeth Urban <lurban5@hotmail.com>
2016-09-16 19:05:04 -0400

    times for running

---
+++ lab-week2/time.py
@@ -0,0 +1,14 @@
+Time for velvet to run:
+low coverage = 0.121 seconds
+high coverage = 13.1055 seconds
+
+time for spades to run:
+low coverage = 4.1 seconds
+high coverage = 1 minute and 57 seconds 
+
+time for lastz to run:
+low coverage = 1.4 seconds
+high coverage = 2.154 seconds 
+
+time for quast to run:
+both coverage = 7.6 seconds 
