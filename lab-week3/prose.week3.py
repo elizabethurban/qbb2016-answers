4ab28894ab4b4d460b39cd6dcf532ab9539fb2ef
Elizabeth Urban <lurban5@hotmail.com>
2016-09-23 18:44:41 -0400

    updated prose with a better explanation

--- lab-week3/prose.week3.py
+++ lab-week3/prose.week3.py
@@ -1,3 +1,10 @@
 Prose for the effects that I saw:
 
-snpEff was not backward compatable to utalize the mouse mm9 genome, therefore I cannot determine the consequence of each filtered variant. 
+snpEff was not backward compatable to utalize the mouse mm9 genome, therefore 
+I cannot determine the consequence of each filtered variant. 
+
+It would have been possible to go back and rerun all prior steps 
+using the mm10 genome, but then our results would not have matched 
+the papers results. Had the program worked we would have extected to see 
+that for each variant in the vcf files we would have seen what the program 
+predicted the variant to be and we could have then compared that to the papers results. 
