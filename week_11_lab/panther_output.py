127d66f2a05ece79de4db64e96f3446960b779c7
Elizabeth Urban <lurban5@hotmail.com>
2016-12-18 20:36:46 -0500

    panther analysis

---
+++ week_11_lab/panther_output.py
@@ -0,0 +1,24 @@
+"""unfortunatly I was unable to run a ttest to determine the significantly different 
+genes between early and late developmental stages according to the paper.
+I was able to determine which were up and down regulated based off of the above
+2 or less than .5 intensity ration that the paper used though. without the ttest
+it was impossible for me to determine the correct gene, and associated cluster of genes
+that was meant to be run through panther. Instead, just to show I can utalize panther
+and understand the output, I chose a gene at random from the upregulated gene output
+list that I obtained from the first level of gene expression analysis. the gene I chose
+was Icam2"""
+
+what I determined about Icam2 from panther is that it is called 
+intercellular adhesion molecule 2, and its main proteins classes are
+as a signaling molecule, as well as a member of the immunogloublin
+superfamily cell adhesion molecules. the GO terms state that is molecular
+function is for receptor and protein binding, it is involved in the biological
+processes of cell adhesion, and it is a cellular component of the membrane. other proteins 
+in this family include icam1, icam4, tcam1, and icam5. 
+
+A potential role of icam2 in differentiation may include cell movememt and adhesion to
+the proper tissues. during development the proper location of cells, and what other cells
+they contact is important to forming functional tissues, and therefore a protein involved
+in cellular adhesion would imply a role during differentiation as a facilitator of proper protein
+contact interactions to hold the differentiating cells in the proper location near the proper
+neighbors to create a functional tissue. 
